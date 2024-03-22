3f3629bf9c01ef22f7660d6d59b8dffd2ff4b76d
Abdulbarikassim <abdicsxhaka10@gmail.com>
2024-03-22 15:15:25 +0300

    create snake class
    create a snake class which contains the init method and create snake
    method: creates the snake with differents segments

---
+++ day-3/main.py
@@ -0,0 +1,42 @@
+from turtle import Turtle,Screen
+from snake import Snake 
+import time
+
+turtle = Turtle()
+screen = Screen()
+
+
+
+snake = Snake()
+
+screen.bgcolor("black")
+screen.title("SNAKE GAME.")
+screen.setup(height=600,width=600)
+screen.tracer(0)
+
+screen.listen()
+
+# screen.onkey(snake.go_up,"Up")
+# screen.onkey(snake.go_down,"Down")
+# screen.onkey(snake.go_left,"Left")
+# screen.onkey(snake.go_right,"Right")
+
+game_is_on = True
+
+while game_is_on:
+  screen.update()
+  time.sleep(0.1)
+  # snake.move()
+  
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
---
+++ day-3/snake.py
@@ -0,0 +1,40 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+turtle = Turtle()
+
+screen.listen()
+
+
+STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT  = 180
+RIGHT = 0
+
+
+class Snake():
+  def __init__(self):
+    self.segments = []
+    self.create_snake()
+    self.head =self.segments[0]
+    
+  def create_snake(self):
+    for position in STARTING_POSITIONS:
+      new_turtle = Turtle("square")
+      new_turtle.color("white")
+      new_turtle.penup()
+      new_turtle.goto(position)
+      self.segments.append(new_turtle)
+      
+  # def move(self):
+  #     for seg_num in range(len(self.segments)-1,0,-1):
+  #       x= self.segments[seg_num-1].xcor()
+  #       y= self.segments[seg_num-1].ycor()
+  #       self.segments[seg_num].goto(x,y)
+        
+  #     self.head.forward(MOVE_DISTANCE)
+      
+            
