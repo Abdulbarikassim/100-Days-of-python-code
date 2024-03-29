153ef711326848b9ffcdd165a12da076b1302b72
Abdulbarikassim <abdicsxhaka10@gmail.com>
2024-03-29 12:29:03 +0300

    Feat: Add move method.
    -Implement the move the to adjust the movement of the ball.

--- pongGame/ball.py
+++ pongGame/ball.py
@@ -9,4 +9,11 @@ class Ball(Turtle):
     self.color("white")
     self.shape("circle")
     self.shapesize(stretch_wid=1)
-        
+    self.penup()
+    self.x_move = 10    
+    self.y_move = 10
+    
+  def move(self):
+    new_y = self.ycor() + self.x_move
+    new_x = self.xcor() + self.y_move
+    self.goto(new_x,new_y)
