f718acc5ebdfe1fba61fbf5a6f9ba8482b936409
Abdulbarikassim <abdicsxhaka10@gmail.com>
2024-03-29 11:34:49 +0300

    Add Paddle class
    -Define a paddlae class that inherits from the Turtle class.
    -Initialize paddle attributes such as shape,color,size , and position.

---
+++ pongGame/paddle.py
@@ -0,0 +1,14 @@
+from turtle import Turtle
+
+
+
+# create the paddle class.
+class Paddle(Turtle):
+  def __init__(self,position):
+    super().__init__() # inherits the Turtles class.
+    self.shape("square")
+    self.color("white")
+    self.penup()
+    self.shapesize(stretch_wid=4,stretch_len=1)
+    self.goto(position)
+    
