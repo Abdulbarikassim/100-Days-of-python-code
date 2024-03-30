9a448d5673fd2262dd756b2163cf2007150ca2ea
Abdulbarikassim <abdicsxhaka10@gmail.com>
2024-03-30 15:21:28 +0300

    Feat: Add resetPosition method
    -This method the position of the ball when the ball misses the paddle.
    -It changes the direction of the ball movement to reverse .

--- pongGame/ball.py
+++ pongGame/ball.py
@@ -24,4 +24,6 @@ class Ball(Turtle):
   def bounce_x(self):  # bounce the ball reverse  to the x -axis.
     self.x_move *= -1 
   
-  
+  def reset_position(self):
+    self.goto(0,0)
+    self.bounce_x()
