d7e897b8d75bea120fb5b2f20efb1984f79bada4
Abdulbarikassim <abdicsxhaka10@gmail.com>
2024-04-07 14:37:30 +0300

    Initail Setup: Initialize Game enivronment.
    -This is where all the game play is done.We import different module of
    the game to be executed in here(car_manager,player and the scoreboard).
    -create a screen object and set the width and height of the object.
    -we use the onexit method to keep the screen on until we click for the
    exit.
    -we use the tracer and update method to remove the animation for the car
    which will be generated.

---
+++ RacingGame/main.py
@@ -0,0 +1,49 @@
+from turtle import Turtle,Screen
+from car_manager import CarManger
+from player import Player
+from scoreboard import Scoreboard
+
+
+screen = Screen()
+screen.setup(width=800,height=600)
+screen.tracer(0)
+
+
+
+car_manager = CarManger()
+
+
+
+
+is_game_on = True
+
+# while is_game_on:
+#   screen.update()
+#   car_manager.create_car()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
+ 
