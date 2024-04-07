d0dbf1f99c6d830a8afcc2923ac30d2685c1debb
Abdulbarikassim <abdicsxhaka10@gmail.com>
2024-04-07 15:45:42 +0300

    Add Scoreboard class
    -Initialize the scoreboard to keep the score of the game and also for
    the game over method.

---
+++ RacingGame/scoreboard.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+FONT = ("Courier", 30, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.score = 0
+        self.color("black")
+        self.penup()
+        self.goto(-250,250)
+        self.write(f"Score : {self.score}",font=FONT)
+        
+    
+    def game_over(self):
+      self.goto(0,0)
+      self.write(f"GAME OVER!",font=FONT)
