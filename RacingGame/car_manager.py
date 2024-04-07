f5f8e2eacd5d3f34e1109f773f6a296eff33b115
Abdulbarikassim <abdicsxhaka10@gmail.com>
2024-04-07 16:08:44 +0300

    Feat: Add level up method.
    This method increase the level of the game to make it more fast.

--- RacingGame/car_manager.py
+++ RacingGame/car_manager.py
@@ -17,13 +17,18 @@ class CarManger():
       new_car.shapesize(stretch_len=2)
       new_car.color(random.choice(COLORS))
       new_car.penup()
-      random_y = random.randint(-280,280)
+      random_y = random.randint(-250,250)
       new_car.goto(360,random_y)
       self.cars.append(new_car)
+      self.speed_up = STARTING_MOVE_DISTANCE
     
   def move(self):
     for cars in self.cars:
-      cars.backward(STARTING_MOVE_DISTANCE)
+      cars.backward(self.speed_up)
+    
+    
+  def level_up(self):
+    self.speed_up += 10
     
     
     
