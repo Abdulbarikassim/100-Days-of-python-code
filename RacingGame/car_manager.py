d94cb3a4aa8e872e98b3642d77caf993d45df058
Abdulbarikassim <abdicsxhaka10@gmail.com>
2024-04-07 15:02:11 +0300

    Add CarManager class
    -generate the car and implement it's movement.
    -Added a CarManager class responible for managing cars in the game.
    -Move method for implenting the movement of the car.

---
+++ RacingGame/car_manager.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+class CarManger():
+  def __init__(self):
+    self.cars = []
+    
+    
+  def create_car(self):
+    random_generate = random.randint(1,6)
+    if random_generate == 1:
+      new_car = Turtle("square")
+      new_car.shapesize(stretch_len=2)
+      new_car.color(random.choice(COLORS))
+      new_car.penup()
+      random_y = random.randint(-280,280)
+      new_car.goto(360,random_y)
+      self.cars.append(new_car)
+    
+  def move(self):
+    for cars in self.cars:
+      cars.backward(STARTING_MOVE_DISTANCE)
+    
+    
+    
+      
