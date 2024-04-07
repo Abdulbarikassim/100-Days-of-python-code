f0c3940e7fbd1a9d259485e9bb40976fd3449bde
Abdulbarikassim <abdicsxhaka10@gmail.com>
2024-04-07 15:36:08 +0300

    Implement Player movement and Collision Detection.
    -Added event binding using onkey method to move the player up.
    -Implement collision detection between player and cars managed by
    car-manager.

--- RacingGame/main.py
+++ RacingGame/main.py
@@ -8,11 +8,12 @@ import time
 screen = Screen()
 screen.setup(width=800,height=600)
 screen.tracer(0)
-
-
+screen.listen()
 
 car_manager = CarManger()
+player = Player()
 
+screen.onkey(player.go_up,"Up")
 
 
 
@@ -24,6 +25,13 @@ while is_game_on:
   car_manager.create_car()
   car_manager.move()
   
+  # check collision of player with cars.
+  
+  for cars in car_manager.cars:
+    if cars.distance(player) < 20 :
+      is_game_on = False
+      
+      
 
 
 
